f6287e9420f6ed52e783bddbf0968f4d32f6e834
ShambhaviSeth <shambhavi22seth@gmail.com>
2024-10-23 16:08:59 -0500

    Update structure

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-m0eqeaygti!b6e1#jbo6jiqsa2&3(-46t2_6atd=t&w71_aut0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["django-env.eba-uu2c976j.us-west-2.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ["django-env.eba-uu2c976j.us-west-2.elasticbeanstalk.com", "127.0.0.1"]
 
 
 # Application definition
